69a7da6ab6599fd6d0f97dff1df548b020ebfd47
AjinyaNaik <ajinkya0210@gmail.com>
2026-06-16 22:04:17 -0400

    Added eslint config files

---
+++ backend/eslint.config.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "commonjs",
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error",
+    },
+  },
+];
