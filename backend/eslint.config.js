33e3e9aac4db53cd03b6140e6c4ff208561625ef
AjinyaNaik <ajinkya0210@gmail.com>
2026-06-16 22:07:07 -0400

    modified eslint.config.js

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,13 +1,8 @@
 export default [
   {
     files: ["**/*.js"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "commonjs",
-    },
     rules: {
       "no-unused-vars": "warn",
-      "no-undef": "error",
     },
   },
 ];
