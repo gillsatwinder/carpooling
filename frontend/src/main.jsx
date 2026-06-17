09dd4f9ad7ce9eb0b33c04293746fef08010c217
AjinyaNaik <ajinkya0210@gmail.com>
2026-06-17 16:35:46 -0400

    Fixed error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 
-import { AuthProvider } from "./context/AuthContext.jsx";
+import { AuthProvider } from "./context/AuthProvider.jsx";
 
 
 createRoot(document.getElementById('root')).render(
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { registerUser } from "../hooks/auth.hooks";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../context/useAuth";
 import { motion } from "framer-motion";
 
 export default function Register() {
